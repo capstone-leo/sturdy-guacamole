a7cd20766abaf3d528d45d3a746de5e8ee5077ce
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-02-03 01:16:12 -0500

    feat(landing page with very basic styling and links to app)

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 const LandingPage = () => {
-	const [showInstructions, setShowInstructions] = useState('false');
+	const [showInstructions, setShowInstructions] = useState(false);
 
 	const showDirections = (event) => {
 		event.preventDefault();
@@ -9,19 +11,50 @@ const LandingPage = () => {
 		setShowInstructions(!showInstructions);
 	};
 	return (
-		<div>
-			<header>Aether</header>
-			<button type='button' onClick={showDirections}>
+		<div style={{ textAlign: 'center', marginTop: '15%', fontSize: '60px' }}>
+			<header>a e t h e r</header>
+			<p style={{ fontSize: '15px' }}>(working title)</p>
+			<button
+				type='button'
+				onClick={showDirections}
+				style={{ textAlign: 'center', marginTop: '10%', fontSize: '15px' }}
+			>
 				view instructions
-			</button>{' '}
-			{showDirections ? (
-				<p>
-					“Never follow anyone else’s path. Unless you’re in the woods and you’re lost
-					and you see a path. Then by all means follow that path.”
-				</p>
+			</button>
+			{showInstructions ? (
+				<ol style={{ textAlign: 'center', marginTop: '0%', fontSize: '15px' }}>
+					<li>“One band. One Sound.”</li>
+					<li>"If you ain't first, you're last" </li>
+					<li>"You shall not pass!"</li>
+					<li>"I am McLovin"</li>
+					<li>
+						"Life is like a box of chocolates, you never know what you're gonna get"
+					</li>
+				</ol>
 			) : null}
-			{/* <button></button>
-			<button></button> */}
+			<br />
+			<Link to='/sesh'>
+				<button type='button' style={{ textAlign: 'center', marginTop: '1%' }}>
+					Start Jamming {'>'}
+				</button>
+			</Link>
+			<br />
+			<Particles
+				params={{
+					particles: {
+						line_linked: {
+							shadow: {
+								enable: true,
+								color: '#3CA9D1',
+								blur: 5
+							}
+						}
+					}
+				}}
+				style={{
+					width: '100%'
+				}}
+			/>
 		</div>
 	);
 };
